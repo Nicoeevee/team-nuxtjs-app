6be0bc03729ac74517c04893c099a0538d2ed3e3
Apo <maoexon@gmail.com>
2021-03-18 21:26:10 +0800

    更新 首页标语

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <h1 style="font-size:10rem">岂止于大</h1>
+    <h1 class="index_slogan"><span>
+      着手开发。
+      立即预览。
+      轻松部署。
+    </span></h1>
     <div class="carousel">
       <el-carousel :interval="5000" indicator-position="outside" arrow="hover" height="560px">
         <el-carousel-item v-for="img in images" :key="img">
@@ -28,10 +32,22 @@ export default {
 </script>
 
 <style>
+.index_slogan {
+  font-size: 9rem;
+}
+
+.index_slogan > span {
+  word-break: normal;
+  width: auto;
+  display: block;
+  line-height: 1em;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  overflow: hidden;
+}
 
 .container {
-  padding-top: 8px;
-  margin: auto;
+  margin: 0;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
