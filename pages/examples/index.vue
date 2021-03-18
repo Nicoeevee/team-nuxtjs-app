8b33f3975cdbb227a47f608534c95fd62d4d87c6
siliangzhongxuhua <1417015655@qq.com>
2021-03-18 13:44:59 +0800

    新建 两个文件

---
+++ pages/examples/index.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="content">
+		<div class="center">
+			<div class="center_left">
+				<div class="center_right">
+						<div ><strong>经典案例</strong>
+						<p>plugins 目录是我们存放自定义插件的地方，自定的插件可以直接在我们的vue代码里面使用，
+							这里如果我们在创建项目的时候选择了UI框架，默认是会把UI框架的配置文件存放到这个目录，
+							比如我这里创建之前选择了elementui，里面会包含一个element-ui.js，代码就跟我们平时用element配置一样,写一个自己的插件也比较简单，
+							比如我这里写了一个时间格式化的插件</p>
+								<div style="margin-top:20px;float:right;">
+									<span>200浏览</span>
+									<span>消息</span>
+									<span>点赞</span>
+								</div>
+						</div>
+						<div>
+						<strong>标题</strong>
+						<p>plugins 目录是我们存放自定义插件的地方，自定的插件可以直接在我们的vue代码里面使用，
+							这里如果我们在创建项目的时候选择了UI框架，默认是会把UI框架的配置文件存放到这个目录，
+							比如我这里创建之前选择了elementui，里面会包含一个element-ui.js，代码就跟我们平时用element配置一样,写一个自己的插件也比较简单，
+							比如我这里写了一个时间格式化的插件</p>
+								<div style="margin-top:20px;float:right;">
+									<span>200浏览</span>
+									<span>消息</span>
+									<span>点赞</span>
+								</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		layout:'home'
+	}
+</script>
+
+<style scoped>
+	.center_right>div{
+		border: 1px solid #cfcfcf;
+		background-color: aliceblue;
+		min-height:150px;
+	}
+	.content{
+		width: 100%;
+		background-color:blue;
+		overflow: hidden;
+	}
+	.center{
+		width: 1100px;
+		margin: 0px auto;
+		overflow: hidden;
+	}
+	.center_left{
+		width: 1200px;
+		margin-right: 20px;
+		min-height: 500px;
+		background-color: pink;
+		float: left;
+		border-radius: 5px;
+		padding: 10px;
+	}
+	.center_right{
+		width: 1000px;
+		min-height: 400px;
+		background-color:#ffffff;
+		float: left;
+		margin-top: 20px;
+		padding: 10px;
+	}
+</style>
---
+++ pages/solution/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="content">
+    <div class="center">
+      <div class="center_left">
+        <div class="article" v-for="i in 6" :key="i">
+          <div class="article_title">
+            标题
+          </div>
+          <div class="article_content">
+            内容
+            <div class="article_footer">
+              <span>200 浏览dd</span>
+              <span>
+                <i class="fa fa-comment" aria-hidden="true"></i>评论
+              </span>
+              <span
+              ><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>点赞</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="center_right">
+        <div class="right_title">
+          推荐
+        </div>
+        <div class="right_item" v-for="o in 6" :key="o">
+          <div class="item_top">
+            <span> <i class="fa fa-calendar-times-o"></i> 2021-3-18 </span>
+            <span><i class="fa fa-eye"></i> 20</span>
+          </div>
+          <div class="item_bootom">
+            内容
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    layout: "home"
+
+  };
+</script>
+<style scoped>
+  .content {
+    width: 100%;
+    background-color: #f9f9f9;
+    overflow: hidden;
+  }
+  .center {
+    margin: 10px auto;
+    width: 1100px;
+    overflow: hidden;
+  }
+  .center_left {
+    width: 780px;
+    margin-right: 20px;
+    min-height: 500px;
+    background-color: #fff;
+    float: left;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 10px;
+  }
+  .center_right {
+    width: 300px;
+    min-height: 500px;
+    background-color: #fff;
+    float: left;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 10px;
+  }
+
+  .article {
+    width: 100%;
+    /* height: 300px; */
+    /* background-color: red; */
+    border-bottom: 1px solid #ddd;
+    margin-bottom: 15px;
+  }
+  .article_title {
+    /* font-size: 20px; */
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .article_footer {
+    text-align: right;
+    margin-top: 20px;
+    margin-bottom: 40px;
+  }
+  .article_footer > span {
+    margin-left: 10px;
+  }
+  .right_title {
+    padding-bottom: 10px;
+    box-sizing: border-box;
+    /* background-color: red; */
+    border-bottom: 1px solid #ddd;
+  }
+  .right_item {
+    padding: 5px;
+    border-bottom: 1px solid #ddd;
+    font-size: 15px;
+    color: #515a6e;
+  }
+  .item_top > span {
+    margin-right: 11px;
+  }
+  .item_bootom {
+    margin-top: 11px;
+    margin-bottom: 11px;
+  }
+</style>
