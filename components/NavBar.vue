4e5fb02e9de743d0622d2384e8e6077f636a4bc1
Apo <maoexon@gmail.com>
2021-03-19 19:54:42 +0800

    修正 底部Footer 位置问题

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,12 +2,13 @@
   <div id="nav-wrapper">
     <div class="nav_container">
       <Logo></Logo>
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"  active-text-color="black" >
+      <el-menu :default-active="activeIndex" active-text-color="black" class="el-menu-demo" mode="horizontal"
+               @select="handleSelect">
         <el-menu-item index="/" @click="$router.push({name: 'index'})">首页</el-menu-item>
         <el-menu-item index="/news" @click="$router.push({name: 'news'})">新闻</el-menu-item>
         <el-menu-item index="/examples" @click="$router.push({name: 'examples'})">案例</el-menu-item>
         <el-menu-item index="/solution" @click="$router.push({name: 'solution'})">疫情防控</el-menu-item>
-<!--        <el-menu-item index="/playground" @click="$router.push({name: 'playground'})">playground</el-menu-item>-->
+        <!--        <el-menu-item index="/playground" @click="$router.push({name: 'playground'})">playground</el-menu-item>-->
       </el-menu>
       <div class="more">
         <el-link type="info"> 加入我们
@@ -15,8 +16,8 @@
         <el-popover
           placement="bottom-end"
           title="二维码"
-          width="100"
-          trigger="hover">
+          trigger="hover"
+          width="100">
           <div style="text-align: right; margin: 0">
             <img :src="qrcode" alt="qrcode"/>
           </div>
@@ -49,7 +50,8 @@ export default {
 
 <style scoped>
 .nav_container {
-  box-shadow: 0 12px 10px -10px rgba(0,0,0,0.12);
+  margin-bottom: 4px;
+  box-shadow: 0 12px 10px -10px rgba(0, 0, 0, 0.12);
   display: flex;
   justify-content: space-around;
   align-items: center;
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,51 @@
 <template>
-  <div class="home_main">
+  <div class="site">
     <NavBar/>
-    <Nuxt class="fadein"/>
-    <div class="footer_main">
-      <Footer></Footer>
-    </div>
-
+    <Nuxt class="fadein site_content"/>
+    <Footer/>
   </div>
 </template>
 <style>
-  .fadein {
-    animation: 1s appear;
-    margin: 0;
-  }
+.site {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
 
-  .home_main {
-    /*position: relative;*/
-    /*z-index: 99;*/
-    display: flex;
-    flex-direction: column;
-  }
+.site_content {
+  flex: 1;
+}
 
-  .footer_main {
-    width: 100%;
-    /*position: absolute;*/
-    /*bottom: 0;*/
-    /*z-index: 1;*/
-  }
+.fadein {
+  animation: 1s appear;
+  margin: 0;
+}
 
-  html {
-    margin: 0;
-    padding: 0;
-    scroll-behavior: smooth;
-    font-family: 'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-    font-size: 16px;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-  }
+html {
+  margin: 0;
+  padding: 0;
+  scroll-behavior: smooth;
+  font-family: 'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
 
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-    margin: 0;
-  }
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
 </style>
